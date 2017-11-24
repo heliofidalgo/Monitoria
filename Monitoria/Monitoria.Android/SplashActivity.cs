45c5716f09576ce4bc4bb2ff0bd190054b38cbe3
Helio Fidalgo <hmnff@hotmail.com>
2017-11-24 13:50:17 -0200

    Comiit initial this project for monitoring

---
+++ Monitoria/Monitoria.Android/SplashActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Monitoria.Droid
+{ // NoHistory serve para proibir o usuario de acessar o historico da app
+    [Activity(Theme="@style/Theme.Splash", MainLauncher =true, NoHistory =true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Thread de sleep de 3s para visualizar melhor
+            System.Threading.Thread.Sleep(3000);
+            // aqui iniciamos a atividade principal
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
