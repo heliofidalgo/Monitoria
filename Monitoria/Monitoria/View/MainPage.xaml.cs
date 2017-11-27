1192b28ad607207ea453471e6666a43d5deefacc
Helio Fidalgo <hmnff@hotmail.com>
2017-11-27 10:00:07 -0200

    Atualização do  projeto. Segmento de implementação do app. Add novo colaborador ao projeto.

--- Monitoria/Monitoria/View/MainPage.xaml.cs
+++ Monitoria/Monitoria/View/MainPage.xaml.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Monitoria.View;
 using Xamarin.Forms;
 
 namespace Monitoria
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+            this.Master = new Menu();
+            this.Detail = new NavigationPage(new Home());
         }
     }
 }
